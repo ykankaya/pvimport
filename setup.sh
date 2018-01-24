be370081b7fee14cb1a3d66ec2633f151e23fd54
trimstray <contact@nslab.at>
2018-01-24 23:37:42 +0100

    setup.sh: set 'pvimport' - signed-off-by: trimstray <contact@nslab.at>

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# To adapt this script to your project, do the following:
-#   - vim: ESC:%s/git-template-full/your-project/g
-#   - sed: set -i 's/git-template-full/your-project/g'
-
 readonly _dir="$(dirname "$(readlink -f "$0")")"
 
 # shellcheck disable=SC2034
@@ -13,11 +9,11 @@ if [[ "$1" == "install" ]] ; then
 
   printf "%s\n" "Create symbolic link to /usr/local/bin"
 
-  if [[ -e "${_dir}/bin/git-template-full" ]] ; then
+  if [[ -e "${_dir}/bin/pvimport" ]] ; then
 
-    if [[ ! -e "/usr/local/bin/git-template-full" ]] ; then
+    if [[ ! -e "/usr/local/bin/pvimport" ]] ; then
 
-      ln -s "${_dir}/bin/git-template-full" /usr/local/bin
+      ln -s "${_dir}/bin/pvimport" /usr/local/bin
 
     fi
 
@@ -25,12 +21,12 @@ if [[ "$1" == "install" ]] ; then
 
   printf "%s\n" "Create man page to /usr/local/man/man8"
 
-  if [[ -e "${_dir}/doc/man8/git-template-full.8" ]] ; then
+  if [[ -e "${_dir}/doc/man8/pvimport.8" ]] ; then
 
-    if [[ ! -e "/usr/local/man/man8/git-template-full.8.gz" ]] ; then
+    if [[ ! -e "/usr/local/man/man8/pvimport.8.gz" ]] ; then
 
-      cp "${_dir}/doc/man8/git-template-full.8" /usr/local/man/man8
-      gzip /usr/local/man/man8/git-template-full.8
+      cp "${_dir}/doc/man8/pvimport.8" /usr/local/man/man8
+      gzip /usr/local/man/man8/pvimport.8
 
     fi
 
@@ -40,17 +36,17 @@ elif [[ "$1" == "uninstall" ]] ; then
 
   printf "%s\n" "Remove symbolic link from /usr/local/bin"
 
-  if [[ -L "/usr/local/bin/git-template-full" ]] ; then
+  if [[ -L "/usr/local/bin/pvimport" ]] ; then
 
-    unlink /usr/local/bin/git-template-full
+    unlink /usr/local/bin/pvimport
 
   fi
 
   printf "%s\n" "Remove man page from /usr/local/man/man8"
 
-  if [[ -e "/usr/local/man/man8/git-template-full.8.gz" ]] ; then
+  if [[ -e "/usr/local/man/man8/pvimport.8.gz" ]] ; then
 
-    rm /usr/local/man/man8/git-template-full.8.gz
+    rm /usr/local/man/man8/pvimport.8.gz
 
   fi
 
